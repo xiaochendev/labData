3d2bccdb0fdec587408474400ad4c48e5af4a20b
xiaochendev <ouerxiao@gmail.com>
2025-08-01 15:40:01 -0400

    added prime time in simple solution

--- pratical_loops/script.js
+++ pratical_loops/script.js
@@ -51,12 +51,37 @@ console.log(`------------------`)
 // Part 2: Prime Time ((No Rush))
 // Now we will move onto something slightly more complex.
 // Context: A prime number is any whole number greater than 1 that cannot be exactly divided by any whole number other than itself and 1. For example, the number 5 is prime because it cannot be divided by 4, 3, or 2; it can only be divided by itself (5) and 1. Similarly, the numbers 7 and 11 are prime. As numbers become larger, determining whether or not they are prime is increasingly difficult, but loops make this process relatively easy!
+    // prime number: i > 1 && when (i % 2 !== 0 && i % 3 !== 0);
 // Write a script that accomplishes the following:
     // Declare an arbitrary number, n.
+    let n = 2;
+    const FIRST_PRIME = 2;
     // Create a loop that searches for the next prime number, starting at n and incrementing from there.
+    while(n < 50) {
+        if(n % 2 !== 0 && n % 3 !== 0) {
+            console.log(`
+                First prime number is: ${FIRST_PRIME}, 
+                Found next prime number ${n}`);
+            break;
+            };
+        n++;
+    }
     // As soon as you find the prime number, log that number and exit the loop.
 
 // Continuing with the example above, if n is equal to 4, your loop should log 5. Similarly, if n is 5, it should log 7, and if n is 9, it should log 11. Test your loop with higher numbers and reference an online prime number table to determine the accuracy of your code.
+    let currentPrime = 7;
+    let nextPrime = currentPrime + 1;
+    while(nextPrime < 50) {
+        if(nextPrime % 2 !== 0 && nextPrime % 3 !== 0) {
+            console.log(`
+                Current prime number is: ${currentPrime}, 
+                Found next prime number ${nextPrime}`);
+            break;
+            };
+        nextPrime++;
+    }
+
+console.log(`------------------`)
 // Be careful! If you set n to a number too large, your loop could take a long time to process.
 
 
