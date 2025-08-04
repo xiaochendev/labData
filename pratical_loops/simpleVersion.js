80caf2f6ebf6c4d240dd58eb9e7e9aca1d3006b4
xiaochendev <ouerxiao@gmail.com>
2025-08-04 13:09:58 -0400

    add Feeling loopy solution w/o used split

--- pratical_loops/simpleVersion.js
+++ pratical_loops/simpleVersion.js
@@ -119,6 +119,44 @@ console.log(`-----Steps by steps with detail explainations for Feeling Loopy`);
         // You do not need to format the data, the following works well.
         // console.log(cell1, cell2, cell3, cell4);
 
+console.log(`----- Not using split`);
+const csvStr = 'ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26';
+let cell1 = "";
+let cell2 = "";
+let cell3 = "";
+let cell4 = "";
+let commas = 0;
+
+for(let i=0; i<csvStr.length-1; i++){
+    if(csvStr[i] == ","){
+        commas++;
+    } else if (csvStr[i] == "\n"){ 
+        console.log(cell1, cell2, cell3, cell4);
+        // Clear previous data for new row
+        commas = 0;
+        cell1 = "";
+        cell2 = "";
+        cell3 = "";
+        cell4 = "";
+    } else {
+        if (commas == 0) {
+            cell1 += csvStr[i];
+        } else if (commas == 1) {
+            cell2 += csvStr[i];
+        } else if (commas == 2) {
+            cell3 += csvStr[i];
+        } else {
+            cell4 += csvStr[i];
+        }
+    }
+    // If last char in string print final cells
+    if(csvStr.length - 1 == i) {
+        console.log(cell1, cell2, cell3, cell4);
+    }
+}
+ 
+
+console.log(`-----------------------`);
 const csvData = 'ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26';
 // const csvData = 'Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232';
 
