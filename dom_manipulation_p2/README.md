b659d8444ffe2ad96fef9682855dcc7eca6b97d3
xiaochendev <ouerxiao@gmail.com>
2025-08-13 14:51:57 -0400

    added dom_manipulation_p2 dir

---
+++ dom_manipulation_p2/README.md
@@ -0,0 +1,29 @@
+# Dom manipulation part II
+- demonstrated the fundamental ways to manipulate the DoM.
+
+# Steps:
+
+1. Open your terminal
+
+2. Copy repository to your local file
+```
+git clone https://github.com/xiaochendev/labData.git
+```
+
+3. Change directory to specific lab, ex. dom_manipulation_p2
+```
+cd dom_manipulation_p2
+```
+
+4. Install VS-code if you don't have
+```
+brew install --cask visual-studio-code
+```
+
+5. Download Live Server(by Ritwick Dey) extension in VS-code
+
+***Its viewable in your browser by right-clicking open with live server in .html***
+
+
+# Technologies
+- Javascript
---
+++ dom_manipulation_p2/scripts/script.js
@@ -0,0 +1,113 @@
+// -------------------------------- DOM manipulations PART I -------------------------------------------------------------|
+// Menu data structure
+var menuLinks = [
+  { text: 'about', href: '/about' },
+  { text: 'catalog', href: '/catalog' },
+  { text: 'orders', href: '/orders' },
+  { text: 'account', href: '/account' },
+];
+
+// Part 1: Getting Started
+// 1. Select and cache the <main> element in a variable named mainEl.
+let mainEL = document.querySelector("main");
+
+// 2. Set the background color of mainEl to the value stored in the --main-bg CSS custom property.
+    // Hint: Assign a string that uses the CSS var() function like this: 'var(--main-bg)'.
+mainEL.style.backgroundColor = `var(--main-bg)`;
+
+// 3. Set the content of mainEl to <h1>DOM Manipulation</h1>. There are a variety of ways to do this; pick whichever one that you think works best in this situation.
+mainEL.innerHTML = `<h1>DOM Manipulation</h1>`;
+
+// 4. Add a class of flex-ctr to mainEl.
+    // Hint: Use the Element.classList API.
+mainEL.classList.add("flex-ctr");
+
+
+// Part 2: Creating a Menu Bar
+// 1. Select and cache the <nav id="top-menu"> element in a variable named topMenuEl.
+let topMenuEl = document.getElementById("top-menu");
+
+// 2. Set the height of the topMenuEl element to be 100%.
+topMenuEl.style.height = `100%`;
+
+// 3. Set the background color of topMenuEl to the value stored in the --top-menu-bg CSS custom property.
+topMenuEl.style.backgroundColor = `var(--top-menu-bg)`;
+
+// 4. Add a class of flex-around to topMenuEl.
+topMenuEl.classList.add("flex-around");
+
+
+// Part 3: Adding Menu Buttons
+// 1. Iterate over the entire menuLinks array and for each "link" object:
+menuLinks.forEach((link) => {
+    // 2. Create an <a> element.
+    const newEL = document.createElement("a");
+    // 3. On the new element, add an href attribute with its value set to the href property of the "link" object.
+    newEL.href = link.href;
+    // 4. Set the new element's content to the value of the text property of the "link" object.
+    newEL.textContent = link.text;
+    // 5. Append the new element to the topMenuEl element.
+    topMenuEl.appendChild(newEL);
+});
+
+
+// // Part 3:
+// // Iterate over the entire menuLinks array and for each "link" object:
+// for(let link of menuLinks){
+//     //  // Create an <a> element.
+//     let menuButton = document.createElement('a');
+
+//     //  // On the new element, add an href attribute with its value set to the href property of the "link" object.
+//     menuButton.setAttribute("href", link.href);
+    
+//     //  // Set the new element's content to the value of the text property of the "link" object.
+//     menuButton.textContent = link.text;
+//     // console.log(menuButton);
+
+//     //  // Append the new element to the topMenuEl element.
+//     topMenuEl.appendChild(menuButton);
+// }
+
+
+// Part 4: Adding Interactivity
+// With the basic structure of the page now generated purely with JavaScript, we have demonstrated the ability to manipulate the DOM in several fundamental ways.
+
+
+// -------------------------------- end of DOM manipulations PART I -------------------------------------------------------|
+
+// -------------------------------- DOM manipulations PART II - event handling -------------------------------------------------------------|
+// Part 2: Adding Additional HTML and CSS
+// In order to facilitate this, add the following additional "sub-menu" <nav> element to the index.html file within your <header> element, 
+// beneath the existing <nav> element, as follows
+            // <header>
+            // 	<nav id="top-menu"></nav>
+            // 	<!-- Add the <nav> element below -->
+            // 	<nav id="sub-menu"></nav>
+            // </header>
+// Other than this change, do not modify index.html in any way.
+// Secondly, add the following to the styles.css file:
+            // header, #top-menu {
+            // 	position: relative;
+            // }
+            // #top-menu {
+            // 	z-index: 20;
+            // }
+            // #sub-menu {
+            // 	width: 100%;
+            // 	z-index: 10;
+            // 	transition: top 0.5s ease-out;
+            // }
+            // #sub-menu a:hover {
+            // 	background-color: var(--top-menu-bg);
+            // }
+            // nav a.active {
+            // 	background-color: var(--sub-menu-bg);
+            // 	color: var(--main-bg);
+            // }
+// Other than this change, do not modify styles.css in any way.
+
+// Part 3: Creating the Submenu
+
+
+
+// -------------------------------- end of DOM manipulations PART II - event handling -------------------------------------------------------|
