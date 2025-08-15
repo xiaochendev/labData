a38767ca0b5858419b00679cf4039763ac5ab606
xiaochendev <ouerxiao@gmail.com>
2025-08-15 13:50:04 -0400

    added basic css style

---
+++ dom/src/script.js
@@ -0,0 +1,15 @@
+// Cache at least one element using selectElementById.
+// Cache at least one element using querySelector or querySelectorAll.
+// Use the parent-child-sibling relationship to navigate between elements at least once (firstChild, lastChild, parentNode, nextElementSibling, etc.).
+// Iterate over a collection of elements to accomplish some task.
+// Create at least one element using createElement.
+// Use appendChild and/or prepend to add new elements to the DOM.
+// Use the DocumentFragment interface or HTML templating with the cloneNode method to create templated content. 
+// Modify the HTML or text content of at least one element in response to user interaction using innerHTML, innerText, or textContent.
+// Modify the style and/or CSS classes of an element in response to user interactions using the style or classList properties.
+// Modify at least one attribute of an element in response to user interaction.
+// Register at least two different event listeners and create the associated event handler functions.
+// Use at least two Browser Object Model (BOM) properties or methods.
+// Include at least one form and/or input with HTML attribute validation.
+// Include at least one form and/or input with DOM event-based validation. (This can be the same form or input as the one above, but should include event-based validation in addition to the HTML attribute validation.)
+
