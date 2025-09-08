4b212b961034e726317d90ca72e4b41b4dc323ed
xiaochendev <ouerxiao@gmail.com>
2025-09-08 14:59:51 -0400

    added data

---
+++ express_server_app/data/users.mjs
@@ -0,0 +1,20 @@
+export const users = [
+  {
+    id: 1,
+    name: "Carey",
+    username: "cyare23",
+    email: "cy23@example.com",
+  },
+  {
+    id: 2,
+    name: "Mikoto",
+    username: "Miiko",
+    email: "mikoto_u@example.com",
+  },
+  {
+    id: 3,
+    name: "Ronald",
+    username: "RonRonRon",
+    email: "mronald@example.com",
+  },
+];
