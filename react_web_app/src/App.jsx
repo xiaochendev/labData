51989c6734b51b0a99315598d04a844ec9a3d1e1
xiaochendev <ouerxiao@gmail.com>
2025-10-06 15:07:47 -0400

    seted up routes

--- react_web_app/src/App.jsx
+++ react_web_app/src/App.jsx
@@ -1,15 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import NavBar from './components/Nav/NavBar';
+
+import MoviePage from './pages/MoviePage';
+import PeoplePage from './pages/PeoplePage';
+import NotFound from './pages/NotFound';
+import HomePage from './pages/HomePage';
 
 function App() {
-
   return (
     <>
-      <h1>Hello</h1>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movie" element={<MoviePage />} />
+        <Route path="/people" element={<PeoplePage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
