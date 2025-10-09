ae8b40592454b743e6426527013101e318e52279
xiaochendev <ouerxiao@gmail.com>
2025-10-09 11:03:28 -0400

    fixed navbar style

--- react_web_app/src/components/Nav/NavBar.jsx
+++ react_web_app/src/components/Nav/NavBar.jsx
@@ -1,27 +1,13 @@
 import { Link } from "react-router-dom";
+import style from './Navbar.module.css'
 
 export default function NavBar() {
-    let style = {
-        padding: 0,
-        width: '100%',
-        margin: 20,
-        display: 'flex',
-        justifyContent: 'space-between',
-        listStyle: 'none'
-    }
-
   return (
-    <nav width={'100%'}>
-      <ul style={style}>
-        <Link to={'/'}>
-          <li>Home</li>
-        </Link>
-        <Link to={'/movie'}>
-          <li>Search</li>
-        </Link>
-        <Link to={'/people'}>
-          <li>Big Fan of Star Wars</li>
-        </Link>
+    <nav className={style.navbar}>
+      <ul className={style.navItems}>
+        <li className={style.left}><Link to="/">Home</Link></li>
+        <li><Link to="/movie">Search</Link></li>
+        <li><Link to="/people">Big Fan of Star Wars</Link></li>
       </ul>
     </nav>
   );
