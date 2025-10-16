db76687875f6a798976dad55b58db70b30b99a2a
xiaochendev <ouerxiao@gmail.com>
2025-10-16 17:59:32 -0400

    added mode to searchParam

--- capstone/frontend/src/pages/AuthPage/AuthPage.jsx
+++ capstone/frontend/src/pages/AuthPage/AuthPage.jsx
@@ -1,17 +1,20 @@
 import RegisterForm from "../../components/AuthForms/RegisterForm";
 import LoginForm from "../../components/AuthForms/LoginForm";
+import { useSearchParams } from "react-router-dom";
 import { useState } from "react";
 
 export default function AuthPage() {
-  const [newUser, setNewUser] = useState(false);
+  const [searchParams] = useSearchParams();
+  const mode = searchParams.get('mode'); // will be 'register' if URL has ?mode=register
+  const [newUser, setNewUser] = useState(mode === 'register');
 
   return (
-    <>
-      {newUser ? (
-        <RegisterForm setNewUser={setNewUser} />
-      ) : (
-        <LoginForm setNewUser={setNewUser} />
-      )}
-    </>
+    <div className="auth-page">
+        {newUser ? (
+          <RegisterForm setNewUser={setNewUser} />
+        ) : (
+          <LoginForm setNewUser={setNewUser} />
+        )}
+    </div>
   );
 }
