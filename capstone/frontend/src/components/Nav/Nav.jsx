74122b2d39882255a580f5ac89caad4f7515954b
xiaochendev <ouerxiao@gmail.com>
2025-10-17 15:54:54 -0400

    removed home link

--- capstone/frontend/src/components/Nav/Nav.jsx
+++ capstone/frontend/src/components/Nav/Nav.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/authContext/authContext";
 
 export default function Nav() {
-  const { user, cookies, logout } = useAuth();
+  const { logout } = useAuth();
   const nav = useNavigate();
 
   function handleLogout() {
@@ -13,18 +13,14 @@ export default function Nav() {
 
   return (
     <nav className={style.nav}>
-      <>
-        <Link to={"/"}>
-          <h3>Home</h3>
-        </Link>
 
         <Link to={"/dashboard"}>
           <h3>dashboard</h3>
         </Link>
-        <a>
-          <h3 onClick={handleLogout}>Logout</h3>
+
+        <a onClick={handleLogout} >
+          <h3>Log Out</h3>
         </a>
-      </>
     </nav>
   );
 }
